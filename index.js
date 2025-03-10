329d9387b40b65fd5962ae8989a1b70901b0b5f8
ashwani singh <ashwanisingh3846@gmail.com>
2025-03-10 20:52:04 +0530

    admin endpoint added

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const { userRouter }= require('./Routes/user');
 const { courseRouter } = require('./Routes/course');
-
+const { adminRouter } = require("./Routes/admin")
 app.use(express.json());
 
 app.post("/api/v1/user", userRouter);
 app.get("/api/v1/course", courseRouter);
-
+app.post("/api/v1/admin" , adminRouter)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
