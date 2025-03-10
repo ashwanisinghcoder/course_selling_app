2b4592b1aa3add669fa7e96e7b88ab31f96fc4b9
ashwani singh <ashwanisingh3846@gmail.com>
2025-03-10 17:22:14 +0530

    backend structire

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const app = express();
 
+
+app.post('/api/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
+})
+
+app.post('/api/signin', (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
+}) 
+
+app.post('/courses', (req, res) => {
+    
+})
+
+app.post('/purchases', (req, res) => {
+    
+})
+app.get('/course/purchase', (req, res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
