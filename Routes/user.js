2379a001bfc2c1bbe8fa5d13fde0d74fd6c7c397
ashwani singh <ashwanisingh3846@gmail.com>
2025-03-11 12:01:21 +0530

    db added

--- Routes/user.js
+++ Routes/user.js
@@ -1,16 +1,15 @@
 
 const { Router } = require("express");
+const { userModel } = require("../db/db");
 const userRouter = Router();
 userRouter.post('/user/signup', (req, res) => {
-    const { name, email, password } = req.body;
-
-    res.json({ name,  email, password });
+   
 })
 
 userRouter.post('/user/signin', (req, res) => {
-    const { name, email, password } = req.body;
-    res.json({ name, email, password });
-}) 
+   
+})
+     
 
 userRouter.post('/user/purchases', (req, res) => {
     res.json({
---
+++ db/db.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema;
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://ashwanisingh:elVZUtternK9kiNa@cluster0.vk9uv.mongodb.net/app100xdevs");
+        console.log("Connected to MongoDB");
+    } catch (err) {                                 
+        console.log(err);
+    }
+};
+connectDB();                            
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstname: String,
+    lastname: String,
+});
+
+const adminSchema = new Schema({ 
+    email: {type: String, unique: true},
+    password: String,
+    firstname: String,
+    lastname: String,
+})
+
+  const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    image: String,
+    creatorID: ObjectId,
+    creator: String,
+  });
+
+  purchaseSchema = new Schema({
+    userID: ObjectId,
+    courseID: ObjectId,
+  });
+  
+  const userModel = mongoose.model("user", userSchema);
+  const adminModel = mongoose.model("admin", adminSchema);
+  const courseModel = mongoose.model("course", courseSchema);
+  const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel: userModel,
+    adminModel: adminModel, 
+    courseModel: courseModel,
+    purchaseModel: purchaseModel,
+};
